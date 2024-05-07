87f8d20ee8d4a4ca3477617a0082c174f429806d
[NumanXdev] <9256fa@gmial.com>
2024-05-07 22:55:59 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector(".menu");
+let sidebar = document.querySelector(".sidebar");
+
+
+menu.addEventListener("click", () => {
+  
+  sidebar.classList.add("sidebarR");
+  sidebar.classList.toggle("sidebarR");
+  
+  
+ 
+});
